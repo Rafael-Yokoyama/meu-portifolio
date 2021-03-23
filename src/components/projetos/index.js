e88f77c824a7562dd2876371701a46177b409143
Rafael <rafaelyokoyama925@gmail.com>
2021-03-23 10:52:23 -0300

    Update index.js

--- src/components/projetos/index.js
+++ src/components/projetos/index.js
@@ -20,91 +20,106 @@ const Projetos = () => {
 
         <div className="row">
           <div className="cards" id="Containerimg">
-      
-              <div className="divimg">
-              <a className="bloquinho" href="https://fariah-braids.netlify.app/" target="_blank">
+            <div className="divimg">
+              <a
+                className="bloquinho"
+                href="https://pizzariayo.netlify.app/"
+                target="_blank"
+              >
                 <span>
-                  <img  className="imagemProjetos"
-                    src= {img}
-                    alt="imagem"
-                   
-                  />
-                </span> 
-                <figcaption>  Website criado sobre Braids e designer de sobrancelhas <p>clique para visitar</p> </figcaption>
-              
+                  <img className="imagemProjetos" src={img4} alt="imagem" />
+                </span>
+                <figcaption>
+                  {" "}
+                  Projeto fictício sobre uma Pizzaria <p>
+                    clique para visitar
+                  </p>{" "}
+                </figcaption>
               </a>
-              </div>
+            </div>
 
-              <div className="divimg">
-              <a className="bloquinho" href="https://rafael-yokoyama.github.io/projeto_food/build/" target="_blank">
+            <div className="divimg">
+              <a
+                className="bloquinho"
+                href="https://fariah-braids.netlify.app/"
+                target="_blank"
+              >
                 <span>
-                  <img  className="imagemProjetos"
-                    src= {img1 }
-                    alt="imagem"
-                   
-                  />
-                </span> 
-                <figcaption>  Projeto Beer & Foods liste comidas <p>clique para visitar</p> </figcaption>
-              
+                  <img className="imagemProjetos" src={img} alt="imagem" />
+                </span>
+                <figcaption>
+                  {" "}
+                  Website criado sobre Braids e designer de sobrancelhas{" "}
+                  <p>clique para visitar</p>{" "}
+                </figcaption>
               </a>
-              </div>
- 
-              <div className="divimg">
-              <a className="bloquinho" href="https://rafael-yokoyama.github.io/Curriculo_Ts//build/" target="_blank">
-                <span>
-                  <img  className="imagemProjetos"
-                    src= {img2 } alt="imagem"
-                   
-                  />
-                </span> 
-                <figcaption>  Projeto Curriculum  apenas um modelo <p>clique para visitar</p> </figcaption>
-              
-              </a>
-              </div>
+            </div>
 
-              <div className="divimg">
-              <a className="bloquinho" href="https://github.com/Rafael-Yokoyama/Projetos-Trilha-Abinbev-React/tree/main/emporio" target="_blank">
+            <div className="divimg">
+              <a
+                className="bloquinho"
+                href="https://rafael-yokoyama.github.io/projeto_food/build/"
+                target="_blank"
+              >
                 <span>
-                  <img  className="imagemProjetos"
-                    src= {img3 } alt="imagem"
-                   
-                  />
-                </span> 
-                <figcaption> Projeto clone Empório da Cerveja <p>clique para visitar</p> </figcaption>
-              
+                  <img className="imagemProjetos" src={img1} alt="imagem" />
+                </span>
+                <figcaption>
+                  {" "}
+                  Projeto Beer & Foods liste comidas <p>
+                    clique para visitar
+                  </p>{" "}
+                </figcaption>
               </a>
-              </div>
-              <div className="divimg">
-              <a className="bloquinho" href="https://pizzariayo.netlify.app/" target="_blank">
-                <span>
-                  <img  className="imagemProjetos"
-                    src= {img4 } alt="imagem"
-                   
-                  />
-                </span> 
-                <figcaption> Projeto fictício sobre uma  Pizzaria <p>clique para visitar</p> </figcaption>
-              
-              </a>
-              </div>
+            </div>
 
-              <div className="divimg">
+            <div className="divimg">
+              <a
+                className="bloquinho"
+                href="https://rafael-yokoyama.github.io/Curriculo_Ts//build/"
+                target="_blank"
+              >
+                <span>
+                  <img className="imagemProjetos" src={img2} alt="imagem" />
+                </span>
+                <figcaption>
+                  {" "}
+                  Projeto Curriculum apenas um modelo <p>
+                    clique para visitar
+                  </p>{" "}
+                </figcaption>
+              </a>
+            </div>
+
+            <div className="divimg">
+              <a
+                className="bloquinho"
+                href="https://github.com/Rafael-Yokoyama/Projetos-Trilha-Abinbev-React/tree/main/emporio"
+                target="_blank"
+              >
+                <span>
+                  <img className="imagemProjetos" src={img3} alt="imagem" />
+                </span>
+                <figcaption>
+                  {" "}
+                  Projeto clone Empório da Cerveja <p>
+                    clique para visitar
+                  </p>{" "}
+                </figcaption>
+              </a>
+            </div>
+
+            <div className="divimg">
               <a className="bloquinho" href="#" target="_blank">
                 <span>
-                  <img  className="imagemProjetos"
-                   
-                   
-                  />
-                </span> 
-                <figcaption> Novidades em breve aguarde    <p> carregando ...</p> </figcaption>
-              
+                  <img className="imagemProjetos" />
+                </span>
+                <figcaption>
+                  {" "}
+                  Novidades em breve aguarde <p> carregando ...</p>{" "}
+                </figcaption>
               </a>
-              </div>
-
-
-
-         
-       
-            
+            </div>
           </div>
         </div>
       </div>
